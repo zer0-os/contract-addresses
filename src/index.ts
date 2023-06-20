a3d9729dea485b37b60ba8450fa148f24a9a21b0
James Earle <j_earle@hotmail.com>
2023-06-20 12:19:36 -0700

    formatting for better typing with consuming packages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { Networks } from "./types";
+import { Protocols } from "./types";
 export * from "./types";
 
 const defaultRegistrar = "0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D";
 
-// TODO re add the typing here for when it's used in other repos!!!
-export const zer0ProtocolAddresses: Networks = {
+export const contractAddresses : Protocols = {
   mainnet: {
     zNS: {
       registrar: defaultRegistrar,
@@ -30,101 +29,138 @@ export const zer0ProtocolAddresses: Networks = {
       lpStakingPool: "0x9E87a268D42B0Aba399C121428fcE2c626Ea01FF",
     },
     // For every sale, the top level domain of the sale is on the default registrar above
-    sales: {
-      moto: {
-        genesis: {
-          domain: "0x0025Dd25E1aD8C35a63d690E48162BBE844C7882",
-          collection: "0x51bd5948CF84a1041d2720F56DEd5e173396FC95"
-        },
+    sales: [
+      {
+        name: "moto",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "genesis",
+            type: "collection",
+            domainContract: "0x0025Dd25E1aD8C35a63d690E48162BBE844C7882",
+            nftContract: "0x51bd5948CF84a1041d2720F56DEd5e173396FC95",
+          },
+        ],
       },
-      pals: {
-        gen: {
-          domain: defaultRegistrar,
-          collection: "0x90a1f4B78Fa4198BB620b7686f510FD476Ec7A0B"
-        },
+      {
+        name: "pals",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "gen",
+            type: "collection",
+            domainContract: defaultRegistrar,
+            nftContract: "0x90a1f4B78Fa4198BB620b7686f510FD476Ec7A0B",
+          },
+        ],
       },
-      wheels: {
-        genesis: defaultRegistrar
+      {
+        name: "wheels",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "genesis",
+            type: "domain",
+            domainContract: defaultRegistrar,
+            nftContract: defaultRegistrar,
+          },
+        ],
       },
-      // TODO Resolve how `wheels_updated` and `kicks_updated` are used (if at all)
-      // Following the zOS UI, wheels exist on the default registrar
-      // wheels_updated: "0x19a55608f360f6Df69B7932dC2F65EDEFAa88Dc2",
-      // kicks_updated: "0x6427CcA6CD4586ec7044Cc138158ba4Eb1633711",
-      kicks: {
-        airwild: {
-          domain: defaultRegistrar,
-          season0: defaultRegistrar,
-          season1: defaultRegistrar,
-          season2: {
-            domain: defaultRegistrar,
-            collection: "0x35D2F3CDAf5e2DeA9e6Ae3553A4CaACBA860A395"
-          }
-        }
+      {
+        name: "beasts",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "wape",
+            type: "collection",
+            domainContract: "0x23682326C87D079F73bd88402efD341E07731aE8",
+            nftContract: "0x05F81F870cBca79E9171f22886b58b5597A603AA",
+          },
+          {
+            name: "wolf",
+            type: "collection",
+            domainContract: "0x23682326C87D079F73bd88402efD341E07731aE8",
+            nftContract: "0x1A178CFD768F74b3308cbca9998C767F4E5B2CF8",
+          },
+        ],
       },
-      cribs: defaultRegistrar,
-      craft: defaultRegistrar,
-      // TODO this address deviates from what is on zOS?
-      // crafts: "0x7bA5faff747a3cA7E4ebe65F64e3EDFAEE136846",
-      beasts: {
-        wape: {
-          domain: "0x23682326C87D079F73bd88402efD341E07731aE8",
-          collection: "0x05F81F870cBca79E9171f22886b58b5597A603AA"
-        },
-        wolf: {
-          domain: "0x23682326C87D079F73bd88402efD341E07731aE8",
-          collection: "0x1A178CFD768F74b3308cbca9998C767F4E5B2CF8",
-        }
+      {
+        name: "kicks",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "airwild",
+            type: "domain",
+            domainContract: defaultRegistrar,
+            subcollections: [
+              {
+                name: "season0",
+                type: "collection",
+                domainContract: defaultRegistrar,
+                nftContract: defaultRegistrar,
+              },
+              {
+                name: "season1",
+                type: "collection",
+                domainContract: defaultRegistrar,
+                nftContract: defaultRegistrar,
+              },
+              {
+                name: "season2",
+                type: "collection",
+                domainContract: defaultRegistrar,
+                nftContract: "0x35D2F3CDAf5e2DeA9e6Ae3553A4CaACBA860A395",
+              },
+            ],
+          },
+        ],
       },
-    },
-  },
-  kovan: {
-    zNS: {
-      registrar: "0xC613fCc3f81cC2888C5Cccc1620212420FFe4931",
-      domainPurchaser: "",
-      subregistrarBeacon: "",
-      znsHub: "",
-      basicController: "0x2EF34C52138781C901Fe9e50B64d80aA9903f730",
-      zDao: "",
-    },
-    zAuction: {
-      zAuction: "0x18A804a028aAf1F30082E91d2947734961Dd7f89",
-      legacyZauction: "0x18A804a028aAf1F30082E91d2947734961Dd7f89",
-    },
-    tokens: {
-      wildToken: "0x50A0A3E9873D7e7d306299a75Dc05bd3Ab2d251F",
-      lootToken: "0xD364C50c33902110230255FE1D730D84FA23e48e",
-    },
-    staking: {
-      stakeFactory: "0x47946797E05A34B47ffE7151D0Fbc15E8297650E",
-      lpToken: "0xD364C50c33902110230255FE1D730D84FA23e48e",
-      wildStakingPool: "0x4E226a8BbECAa435d2c77D3E4a096F87322Ef1Ae",
-      lpStakingPool: "0x9CF0DaD38E4182d944a1A4463c56CFD1e6fa8fE7",
-    },
-    sales: {},
-  },
-  rinkeby: {
-    zNS: {
-      registrar: "0xa4F6C921f914ff7972D7C55c15f015419326e0Ca",
-      domainPurchaser: "",
-      znsHub: "0x90098737eB7C3e73854daF1Da20dFf90d521929a",
-      basicController: "0x1188dD1a0F42BA4a117EF1c09D884f5183D40B28",
-      zDao: "0x73D44dEa3A3334aB2504443479aD531FfeD2d2D9",
-    },
-    zAuction: {
-      zAuction: "0xb2416Aed6f5439Ffa0eCCAaa2b643f3D9828f86B",
-      legacyZAuction: "0x376030f58c76ECC288a4fce8F88273905544bC07",
-    },
-    tokens: {
-      wildToken: "0x3Ae5d499cfb8FB645708CC6DA599C90e64b33A79",
-      lootToken: "0x5bAbCA2Af93A9887C86161083b8A90160DA068f2",
-    },
-    staking: {
-      stakeFactory: "0x7bc9fcDD7E156A8d32e8ED602F4e8F06561f4570",
-      lpToken: "0x123c1B5A87E4E11663F2604f3EbCAf4ba86e25E1",
-      wildStakingPool: "0x8a2Bc679FA92989ab6ef037892a994cD8E949545",
-      lpStakingPool: "0x77f854A1AF7f26bbF6626AD75DEC6Eb2D55BA75D",
-    },
-    sales: {},
+      {
+        name: "craft",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "genesis",
+            type: "collection",
+            domainContract: defaultRegistrar,
+            nftContract: defaultRegistrar,
+          },
+        ],
+      },
+      {
+        name: "cribs",
+        type: "domain",
+        domainContract: defaultRegistrar,
+        subcollections: [
+          {
+            name: "wiami",
+            type: "domain",
+            domainContract: defaultRegistrar,
+            subcollections: [
+              {
+                name: "southbeach",
+                type: "domain",
+                domainContract: defaultRegistrar,
+                subcollections: [
+                  {
+                    name: "qube",
+                    type: "collection",
+                    domainContract: defaultRegistrar,
+                    nftContract: defaultRegistrar,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
   goerli: {
     zNS: {
@@ -150,7 +186,6 @@ export const zer0ProtocolAddresses: Networks = {
       wildStakingPool: "0x376030f58c76ECC288a4fce8F88273905544bC07",
       lpStakingPool: "0xCa0F071fcf5b36436F75E422b5Bd23666015b9f9",
     },
-    sales: {
-    },
+    sales: [],
   },
 };
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,45 @@
-export interface Networks {
-  [networkName: string]: {
-    zNS: {
-      [projects: string]: string;
-    },
-    zAuction: {
-      [projects: string]: string;
-    },
-    tokens: {
-      [projects: string]: string;
-    },
-    staking: {
-      [projects: string]: string;
-    },
-    sales: {
-      [projects: string]: string | {
-        [collection: string]: string | {
-          [subcollection: string]: string | {
-            [subcollection: string]: string
-          }
-        }
-      }
-    }
-  }
+type Maybe<T> = T | undefined;
+
+interface ZNS {
+  registrar : string;
+  domainPurchaser : string;
+  subregistrarBeacon : string;
+  znsHub : string;
+  basicController : string;
+  zDAO : string;
+}
+
+interface zAuction {
+  zAuction : string;
+  legacyZAuction : string;
+}
+
+interface Tokens {
+  wildToken : string;
+  lootToken : string;
+  zeroToken : string;
+}
+interface Staking {
+  stakeFactory : string;
+  lpToken : string;
+  wildStakingPool : string;
+  lpStakingPool : string;
+}
+
+interface Collection {
+  name : string;
+  type : "domain" | "collection";
+  domainContract : string;
+  nftContract ?: Maybe<string>;
+  subcollections ?: Maybe<Array<Collection>>;
+}
+
+export interface Protocols {
+  [network : string] : {
+    zNS : ZNS;
+    zAuction : zAuction;
+    tokens : Tokens;
+    staking : Staking;
+    sales : Array<Collection>;
+  };
 }
